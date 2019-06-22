7bbf20342f3be5dd0a025e5e6a3f5b7a7c22b51e
João Gardenberg <joaogardenberg@gmail.com>
2019-06-22 15:06:24 -0300

    Adding ci

--- 04-testing/tests/header.test.js
+++ 04-testing/tests/header.test.js
@@ -1,34 +1,34 @@
-const puppeteer = require('puppeteer');
+const Page = require('./helpers/page');
 
-let browser, page;
+let page;
 
 beforeEach(async () => {
-  browser = await puppeteer.launch({
-    headless: false
-  });
-
-  page = await browser.newPage();
-  await page.goto('localhost:3000');
+  page = await Page.build();
+  await page.goto('http://localhost:3000');
 });
 
 afterEach(async () => {
-  await browser.close();
+  await page.close();
 });
 
 test('the header has the correct text', async () => {
-  try {
-    const text = await page.$eval('a.brand-logo', (el) => el.innerHTML);
-  } catch (e) {}
+  const text = await page.getContentsOf('a.brand-logo');
 
   expect(text).toEqual('Blogster');
 });
 
 test('clicking login starts oAuth flow', async () => {
-  try {
-    await page.click('.right a');
-  } catch(e) {}
+  await page.click('.right a');
 
   const url = await page.url();
 
   expect(url).toMatch(/accounts\.google\.com/);
+});
+
+test('when signed in, shows logout button', async () => {
+  await page.login();
+
+  const text = await page.getContentsOf('a[href="/auth/logout"]');
+
+  expect(text).toEqual('Logout');
 });
